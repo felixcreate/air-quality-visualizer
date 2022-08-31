1f9ab891644ad58670f2c85f207fef329d206164
felixcreate <61338581+felixcreate@users.noreply.github.com>
2022-08-30 21:47:43 -0400

    Updated packaging to work on macOS

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Run the goal javafx:run
 
 ## To package:
  - Use the correct platform profile (windows or macos)
- - Run the goals clean and install (eg. mvn clean install)  
+ - Run the goals clean and install (e.g. mvn clean install)
+ - Example for windows: mvn clean install -P platform-windows
 
 The packaged app and runtime should be in target/installer
--- package.sh
+++ package.sh
@@ -3,5 +3,5 @@ then
   "${JAVA_HOME}"/bin/jpackage.exe -t app-image --dest target/installer --name "Air Quality Data Packager" -m org.felixfb.airquality.exportedjsontoxlsx/org.felixfb.airquality.exportedjsontoxlsx.Start --java-options -Xmx2048m --runtime-image target/runtime --app-version "${PROJECT_VERSION}" --icon icons/icon.ico
 elif [ "${OS_TYPE}" = "macos" ]
 then
-  "${JAVA_HOME}"/bin/jpackage.exe -t app-image --dest target/installer --name "Air Quality Data Packager" -m org.felixfb.airquality.exportedjsontoxlsx/org.felixfb.airquality.exportedjsontoxlsx.Start --java-options -Xmx2048m --runtime-image target/runtime --app-version "${PROJECT_VERSION}" --icon icons/icon.icns --mac-package-name "Air Quality Data Packager"
+  "${JAVA_HOME}"/bin/jpackage -t app-image --dest target/installer --name "Air Quality Data Packager" -m org.felixfb.airquality.exportedjsontoxlsx/org.felixfb.airquality.exportedjsontoxlsx.Start --java-options -Xmx2048m --runtime-image target/runtime --app-version "${PROJECT_VERSION}" --icon icons/icon.icns --mac-package-name "Air Quality Data Packager"
 fi
