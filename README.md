54daeaa3629276760a5ce62165b8fdf4df0b0c74
felixcreate <61338581+felixcreate@users.noreply.github.com>
2022-08-31 17:18:44 -0400

    Update maven to automatically use correct profile based on os family

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 ## To run using maven:
-Run the goal javafx:run
+ - Run the goal javafx:run
 
 ## To package:
- - Use the correct platform profile (windows or macos)
  - Run the goals clean and install (e.g. mvn clean install)
- - Example for windows: mvn clean install -P platform-windows
+
 
 The packaged app and runtime should be in target/installer
--- package.sh
+++ package.sh
@@ -4,4 +4,6 @@ then
 elif [ "${OS_TYPE}" = "macos" ]
 then
   "${JAVA_HOME}"/bin/jpackage -t app-image --dest target/installer --name "Air Quality Data Packager" -m org.felixfb.airquality.exportedjsontoxlsx/org.felixfb.airquality.exportedjsontoxlsx.Start --java-options -Xmx2048m --runtime-image target/runtime --app-version "${PROJECT_VERSION}" --icon icons/icon.icns --mac-package-name "Air Quality Data Packager"
+else
+  exit 1
 fi
